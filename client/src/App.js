490b66cfe360434f6fa3c56236906f41492ef6f5
kjmatt91 <kjmatt91@gmail.com>
2021-09-21 21:05:12 -0400

    completed part 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Search from './components/candies'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <Search/>
     </div>
   );
 }
--- client/src/components/candies.js
+++ client/src/components/candies.js
@@ -1,13 +1,38 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import axios from 'axios'
 
 const Search = () => {
     const [searchTerm, setSearchTerm] = useState("")
+    const [candyMakers, setCandyMakers] = useState([])
+    const [filteredCandyMakers, setFilteredCandyMakers] = useState([])
+
+    useEffect(() => {
+      fetchCandyMakers()
+    },[])
+
+    useEffect(() => {
+      setFilteredCandyMakers(candyMakers.filter(maker => maker.name.toLowerCase().includes(searchTerm.toLowerCase()) || maker.country.toLowerCase().includes(searchTerm.toLowerCase())))
+    },[searchTerm])
+
+
+    const fetchCandyMakers = async () => {
+      let {data} = await axios.get("http://localhost:1337/api/manufacturers")
+      setCandyMakers(data)
+      setFilteredCandyMakers(data)
+      
+    }
 
     return (
         <div className="content">
             <div className="title">Candy Makers</div>
             <input className="searchBox" type="text" name="search" value={searchTerm} onChange={event => setSearchTerm(event.target.value)} />
             <div className="searchTerm">{searchTerm}</div>
+
+            {filteredCandyMakers.map(maker => {
+              return (
+                <div>{`${maker.name} (${maker.country})`}</div>
+              )
+            })}
         </div>
     )
 }
